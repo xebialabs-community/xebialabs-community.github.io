9a74f066b8c12a1a0757f55d3bf8e16229269f35
zvercodebender <rbroker@xebialabs.com>
2019-04-30 14:50:13 -0400

    new token

--- .travis-scripts/push.sh
+++ .travis-scripts/push.sh
@@ -18,7 +18,7 @@ upload_files() {
   #git remote add origin-pages https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git > /dev/null 2>&1
   #git remote add origin https://oauth2:${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git
   #git push origin HEAD:master
-  git push -q https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.git master
+  git push -q https://${TRAVIS_PUSH}@github.com/zvercodebender/xebialabs-community.git master
 }
 
 setup_git
