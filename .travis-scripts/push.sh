fbe6704d531c79e0cbbad38756094d16a1bfd376
zvercodebender <rbroker@xebialabs.com>
2019-04-30 13:35:00 -0400

    push change

--- .travis-scripts/push.sh
+++ .travis-scripts/push.sh
@@ -9,13 +9,12 @@ setup_git() {
 
 commit_website_files() {
   #git checkout -b master
-  git checkout
   git add . *.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  #git remote add origin-pages https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git > /dev/null 2>&1
   git push
 }
 
