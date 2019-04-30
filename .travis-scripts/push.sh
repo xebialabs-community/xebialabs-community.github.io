debd3d1e3dadff4e8d45f29561f7ce7b15515e54
zvercodebender <rbroker@xebialabs.com>
2019-04-30 12:57:05 -0400

    ok

--- .travis-scripts/push.sh
+++ .travis-scripts/push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+  git push 
 }
 
 setup_git
