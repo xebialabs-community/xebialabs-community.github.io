09cf584e0dba3bca069590aa53f373fe72df9a9a
zvercodebender <rbroker@xebialabs.com>
2019-04-30 14:12:38 -0400

    try using the token

--- .travis-scripts/push.sh
+++ .travis-scripts/push.sh
@@ -15,7 +15,7 @@ commit_website_files() {
 
 upload_files() {
   #git remote add origin-pages https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git > /dev/null 2>&1
-  git remote add origin https://${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git
+  git remote add origin https://oauth2:${GH_TOKEN}@github.com/zvercodebender/xebialabs-community.github.io.git
   git push origin HEAD:master
 }
 
